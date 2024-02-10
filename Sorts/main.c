98d62ea7443f41b3aacf6a2fe51ae31a2c4fe463
Yaroslav Korotchik <kk2eley@gmail.com>
2024-02-10 17:44:03 +0300

    Add functions of generating not increasing and random arrays

--- Sorts/main.c
+++ Sorts/main.c
@@ -19,27 +19,24 @@ void *inordered(int n, long long int *a) {
         a[i] = a[i - 1] + step;
     }
 }
-/*
+
 void *postordered(int n, long long int *a) {
     srand(time(NULL));
-    long long int *arr = malloc(n * sizeof(long long int));
-    long long int step = (LLONG_MAX - LLONG_MIN) / n;
-    int koef = 1;
-    if (step >  RAND_MAX)
-        koef = step /  RAND_MAX + 1;
-    arr[n-1] = LLONG_MIN + rand() * koef % step;
-    for (int i = n - 2; i >=0; i--)
-        arr[i] = arr[i + 1] + rand() * koef % step;
+    long long int lim = LLONG_MAX / n * 2;
+    long long int step = (((long long)rand() << 32) | rand()) % lim;
+    a[n - 1] = LLONG_MIN + step;
+    for (int i = n - 2; i >= 0; i--) {
+        step = (((long long)rand() << 32) | rand()) % lim;
+        a[i] = a[i - 1] + step;
+    }
 }
 
 void *randomed(int n, long long int *a) {
     srand(time(NULL));
-    long long int *arr = malloc(n * sizeof(long long int));
-    int koef = (LLONG_MAX - LLONG_MIN) / RAND_MAX + 1;
     for (int i = 0; i < n; i++)
-        arr[i] = rand() * koef % LLONG_MAX;
+        a[i] = ((long long int)rand() << 32) | rand();
 }
-*/
+
 /*
 void qsort(int n, long long int *a) {
 
@@ -62,18 +59,24 @@ int main(void) {
     long long int *arr1 = malloc(n * sizeof(long long int));
     inordered(n, arr1);
     arrprint(n, arr1);
-/*
+
+    printf("\n================================================================================\n");
+
     long long int *arr2 = malloc(n * sizeof(long long int));
     postordered(n, arr2);
     arrprint(n, arr2);
+
+    printf("\n================================================================================\n");
 
     long long int *arr3 = malloc(n * sizeof(long long int));
     randomed(n, arr3);
     arrprint(n, arr3);
 
+    printf("\n================================================================================\n");
+
     long long int *arr4 = malloc(n * sizeof(long long int));
     randomed(n, arr4);
     arrprint(n, arr4);
-*/
+
     return 0;
 }
