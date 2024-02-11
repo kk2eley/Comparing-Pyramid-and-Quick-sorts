b68d09d681a75d3b66fa36199c1bd9148d7ec202
Yaroslav Korotchik <kk2eley@gmail.com>
2024-02-11 22:22:52 +0300

    Add qsort function

--- Sorts/main.c
+++ Sorts/main.c
@@ -3,11 +3,6 @@
 #include <time.h>
 #include <limits.h>
 
-/*long long int abs(long long int x) {
-    if (x < 0)
-        return -x;
-    return x;
-}*/
 
 void *inordered(int n, long long int *a) {
     srand(time(NULL));
@@ -37,45 +32,80 @@ void *randomed(int n, long long int *a) {
         a[i] = ((long long int)rand() << 32) | rand();
 }
 
-/*
-void qsort(int n, long long int *a) {
+int swp = 0;
+int cmp = 0;
 
+void swap(long long int* a, long long int* b) {
+    swp++;
+    long long int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
+long long int compare(long long int a, long long int b) {
+    cmp++;
+    return ((a < 0) ? -a : a) - ((b < 0) ? -b : b);
+}
+
+
+int partition(int n, long long int *a) {
+    long long int pivot = a[n/2];
+    int i = 0, j = n - 1;
+    do{
+        while (a[i] < pivot)
+            i++;
+        while (a[j] > pivot)
+            j--;
+        if (i <= j) {
+            swap(&a[i], &a[j]);
+            i++;
+            j--;
+        }
+    } while (i <= j);
+    return i;
+}
+
+void qsortCustom(int n, long long int *a) {
+    if (n > 2) {
+        int rif = partition(n, a);
+        qsortCustom(rif , a);
+        qsortCustom(n - rif, a + rif);
+    } else if (n == 2 && a[0] > a[1]) {
+        swap(&a[0], &a[1]);
+    }
+}
+
+/*
 void psort(int n, long long int *a) {
 
 }*/
 
 void arrprint(int n, long long int *a) {
     for (int i = 0; i < n; i++) {
-        printf("%25lld ", a[i]);
-        if ((i + 1) % 3 == 0) printf("\n");
+        printf("%5lld ", a[i]);
+        if ((i + 1) % n == 0) printf("\n");
     }
 }
 
 int main(void) {
-    int n = 10;
-
+    int n = 2;
+/*
     long long int *arr1 = malloc(n * sizeof(long long int));
     inordered(n, arr1);
-    arrprint(n, arr1);
-
-    printf("\n================================================================================\n");
 
     long long int *arr2 = malloc(n * sizeof(long long int));
     postordered(n, arr2);
-    arrprint(n, arr2);
-
-    printf("\n================================================================================\n");
 
     long long int *arr3 = malloc(n * sizeof(long long int));
     randomed(n, arr3);
-    arrprint(n, arr3);
-
-    printf("\n================================================================================\n");
+*/
 
     long long int *arr4 = malloc(n * sizeof(long long int));
     randomed(n, arr4);
+    for (int i = 0; i < n; i++)
+        arr4[i] %= 100;
+    arrprint(n, arr4);
+    qsortCustom(n, arr4);
     arrprint(n, arr4);
 
     return 0;
